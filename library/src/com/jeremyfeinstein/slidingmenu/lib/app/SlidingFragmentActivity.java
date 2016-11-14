18f60b8a0fbb5af441b513f9e92469a23b968931
Zilla Chen <zillachan@gmail.com>
2016-11-14 16:09:50 +0800

    fix FloatMath.sin(f);error
    fix FloatMath.sin(f);error

--- library/src/com/jeremyfeinstein/slidingmenu/lib/app/SlidingFragmentActivity.java
+++ library/src/com/jeremyfeinstein/slidingmenu/lib/app/SlidingFragmentActivity.java
@@ -2,13 +2,14 @@ package com.jeremyfeinstein.slidingmenu.lib.app;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
-public class SlidingFragmentActivity extends FragmentActivity implements SlidingActivityBase {
+public class SlidingFragmentActivity extends AppCompatActivity implements SlidingActivityBase {
 
 	private SlidingActivityHelper mHelper;
 
